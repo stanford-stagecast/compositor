bcfe1288cb97130d34557e91a8eab2c3c789b03a
Richard Lin <yifengl@stanford.edu>
2021-01-21 13:46:30 -0800

    Add comments for ThreadPool usage

--- src/util/thread_pool.hh
+++ src/util/thread_pool.hh
@@ -1,5 +1,16 @@
 /* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 
+/* The ThreadPool class executes image processing tasks using multiple threads.
+   All the threads synchronize with each other to ensure a task is compeleted
+   before moving on to the next task.
+
+   Modules using ThreadPool should declare a ThreadPool<Module> object, then
+   append tasks to be compeleted. All tasks are functions with the format:
+   function_name( const uint16_t, const uint16_t ). The appended functions
+   have access to the module's private variables. The tasks don't take inputs
+   or output, so all data should be stored as private variables in the
+   given module. */
+
 #ifndef THREAD_POOL_HH
 #define THREAD_POOL_HH
 
@@ -30,14 +41,14 @@ private:
   bool output_complete_ { false };
 
   void process_rows( const uint8_t id );
+  const int Start { 0 };
+  int End { 0 };
   // Only return when all the threads completed their current level work
   // For N tasks, each task is synchronized based on their level:
   // Start: 0
   // Task1: 1
   // ...
   // End: N + 1
-  const int Start { 0 };
-  int End { 0 };
   void synchronize_threads( const uint8_t id, const int level );
   ThreadPool( const ThreadPool& ) = delete;
   ThreadPool& operator=( const ThreadPool& ) = delete;
