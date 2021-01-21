8a678f9eff2005f95cf50ba425508e9bba5f8a1e
Richard Lin <yifengl@stanford.edu>
2021-01-21 11:41:22 -0800

    Refactor compositor to use ThreadPool

--- src/util/compositor.cc
+++ src/util/compositor.cc
@@ -13,27 +13,12 @@ Compositor::Compositor( const uint16_t width,
   : width_( width )
   , height_( height )
   , thread_count_( thread_count )
-  , threads_( thread_count_ )
-  , output_level_( thread_count_, Start )
+  , pool_( thread_count, width, height, this )
 {
-  for ( uint8_t i = 0; i < thread_count_; i++ ) {
-    threads_[i] = std::thread( &Compositor::process_rows, this, i );
-  }
+  pool_.append_task( &Compositor::composite_task );
 }
 
-Compositor::~Compositor()
-{
-  {
-    lock_guard<mutex> lock( lock_ );
-    thread_terminate_ = true;
-  }
-  cv_threads_.notify_all();
-  for ( auto& t : threads_ ) {
-    t.join();
-  }
-}
-
-void Compositor::process_pixel( const uint16_t row, const uint16_t col )
+void Compositor::composite_pixel( const uint16_t row, const uint16_t col )
 {
   double remaining_alpha = 1.0;
   output_raster_.R().at( col, row ) = 0;
@@ -60,80 +45,19 @@ void Compositor::process_pixel( const uint16_t row, const uint16_t col )
   }
 }
 
-void Compositor::synchronize_threads( const uint8_t id, const Level level )
+void Compositor::composite_task( const uint16_t row_start_idx,
+                                 const uint16_t row_end_idx )
 {
-  unique_lock<mutex> lock( lock_ );
-  output_level_[id] = level;
-  // If this is the last thread to finish, wake up all threads, otherwise wait
-  // until other threads finished
-  if ( accumulate( output_level_.begin(), output_level_.end(), 0 )
-       == thread_count_ * level ) {
-    // Manually unlock the lock to avoid waking up the waiting threads only to
-    // block again
-    input_ready_ = false;
-    lock.unlock();
-    cv_threads_.notify_all();
-  } else {
-    cv_threads_.wait( lock, [&] {
-      return accumulate( output_level_.begin(), output_level_.end(), 0 )
-               >= thread_count_ * level
-             || ( level == End && output_level_[id] == Start );
-    } );
-  }
-}
-
-void Compositor::process_rows( const uint8_t id )
-{
-  const uint16_t num_rows = height_ / thread_count_;
-  const uint16_t row_start_idx = id * num_rows;
-  const uint16_t row_end_idx = row_start_idx + num_rows;
-  while ( true ) {
-    {
-      unique_lock<mutex> lock( lock_ );
-      cv_threads_.wait( lock,
-                        [&] { return input_ready_ || thread_terminate_; } );
-      if ( thread_terminate_ ) {
-        return;
-      }
-    } // End of lock scope
-
-    for ( int row = row_start_idx; row < row_end_idx; row++ ) {
-      for ( int col = 0; col < output_raster_.width(); col++ ) {
-        process_pixel( row, col );
-      }
+  for ( int row = row_start_idx; row < row_end_idx; row++ ) {
+    for ( int col = 0; col < output_raster_.width(); col++ ) {
+      composite_pixel( row, col );
     }
-    synchronize_threads( id, Compose );
-    // Ensure only one thread marks the job as done and wakes up main thread
-    {
-      unique_lock<mutex> lock( lock_ );
-      if ( !output_complete_ ) {
-        output_complete_ = true;
-      }
-    } // End of lock scope
-    synchronize_threads( id, End );
-    cv_main_.notify_one();
   }
 }
 
 RGBRaster& Compositor::composite()
 {
-  {
-    lock_guard<mutex> lock( lock_ );
-    input_ready_ = true;
-  }
-  cv_threads_.notify_all();
-
-  unique_lock<mutex> lock( lock_ );
-  cv_main_.wait( lock, [&] {
-    return output_complete_
-           && accumulate( output_level_.begin(), output_level_.end(), 0 )
-                == thread_count_ * End;
-  } );
-  // Reset all internal states
-  output_complete_ = false;
-  input_ready_ = false;
-  for ( Level& level : output_level_ ) {
-    level = Start;
-  }
+  pool_.input_complete();
+  pool_.wait_for_result();
   return output_raster_;
 }
--- src/util/compositor.hh
+++ src/util/compositor.hh
@@ -11,43 +11,27 @@
 #include "input/jpeg.hh"
 #include "util/chroma_key.hh"
 #include "util/raster.hh"
+#include "util/thread_pool.hh"
 
 class Compositor
 {
 private:
   uint16_t width_, height_;
+  int thread_count_;
+  ThreadPool<Compositor> pool_;
   // The rasters are ordered by depth
-  // The first raster is displayed on top and the last is background
+  // The raster at index 0 is displayed on top and the last is background
   std::vector<RGBRaster*> rasters_ {};
   RGBRaster output_raster_ { width_, height_, width_, height_ };
 
-  // For threading
-  uint8_t thread_count_;
-  std::vector<std::thread> threads_;
-  std::mutex lock_ {};
-  std::condition_variable cv_threads_ {};
-  std::condition_variable cv_main_ {};
-  bool thread_terminate_ { false };
-  bool input_ready_ { false };
-  enum Level
-  {
-    Start = 0,
-    Compose = 1,
-    End = 2
-  };
-  std::vector<Level> output_level_;
-  bool output_complete_ { false };
-  // Only return when all the threads completed their previous work
-  void synchronize_threads( const uint8_t id, const Level level );
-
-  void process_pixel( const uint16_t row, const uint16_t col );
-  void process_rows( const uint8_t id );
+  void composite_pixel( const uint16_t row, const uint16_t col );
+  void composite_task( const uint16_t row_start_idx,
+                       const uint16_t row_end_idx );
 
 public:
   Compositor( const uint16_t width,
               const uint16_t height,
               const uint8_t thread_count = 2 );
-  ~Compositor();
   std::vector<RGBRaster*>& raster_list() { return rasters_; }
 
   RGBRaster& composite();
